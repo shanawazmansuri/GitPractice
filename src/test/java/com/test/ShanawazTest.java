f66ffb7573c94776b697ce4619ded8bed3be0cb0
shanawaz.mansuri <shanawaz.mansuri@gmail.com>
2021-02-14 16:53:56 +0530

    Changes made by Shanawaz

--- src/test/java/com/test/ShanawazTest.java
+++ src/test/java/com/test/ShanawazTest.java
@@ -5,12 +5,18 @@ public class ShanawazTest {
 	public static void main(String[] args) {
 		
 		ShanawazTest.test1();
+		test2AddedByShanawaz();
 
 	}
 	
 	public static void test1()
 	{
 		System.out.println("This is Shanawaz Test 1");
+	}
+	
+	public static void test2AddedByShanawaz()
+	{
+		System.out.println("This is Shanawaz Test 2");
 	}
 
 }
