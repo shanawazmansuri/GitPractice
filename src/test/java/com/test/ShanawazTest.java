0ab80de2c8c47e4b2d76299cd2af2f370f409c2d
shanawazmansuri <shanawaz.mansuri@gmail.com>
2021-02-14 17:28:44 +0530

    Shanawaz local (#3)
    * Changes made by Shanawaz
    
    * Added test2 by Shanawaz
    
    * Test3 added by Shanawaz

--- src/test/java/com/test/ShanawazTest.java
+++ src/test/java/com/test/ShanawazTest.java
@@ -3,20 +3,24 @@ package com.test;
 public class ShanawazTest {
 
 	public static void main(String[] args) {
-		
+
 		ShanawazTest.test1();
 		test2AddedByShanawaz();
 
+		test3AddedByShanawaz();
+
 	}
-	
-	public static void test1()
-	{
+
+	public static void test1() {
 		System.out.println("This is Shanawaz Test 1");
 	}
-	
-	public static void test2AddedByShanawaz()
-	{
+
+	public static void test2AddedByShanawaz() {
 		System.out.println("This is Shanawaz Test 2");
+	}
+
+	public static void test3AddedByShanawaz() {
+		System.out.println("This is Shanawaz Test 3");
 	}
 
 }
