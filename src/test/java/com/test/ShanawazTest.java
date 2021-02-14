b4599804a0e2c62a578b176d9cd871cdee8a88c8
shanawaz.mansuri <shanawaz.mansuri@gmail.com>
2021-02-14 16:32:17 +0530

    initial commit

---
+++ src/test/java/com/test/ShanawazTest.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class ShanawazTest {
+
+	public static void main(String[] args) {
+		
+		ShanawazTest.test1();
+
+	}
+	
+	public static void test1()
+	{
+		System.out.println("This is Shanawaz Test 1");
+	}
+
+}
